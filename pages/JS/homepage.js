d9f8470da41df2d0b81d66070161e80382ceb2e4
Jeltai <101892796+Jeltai@users.noreply.github.com>
2024-10-07 19:37:03 +0200

    update

--- pages/JS/homepage.js
+++ pages/JS/homepage.js
@@ -52,7 +52,7 @@ const firebaseConfig = {
     localStorage.removeItem('loggedInUserId');
     signOut(auth)
     .then(()=>{
-        window.location.href='index.html';
+        window.location.href='../../index.html';
     })
     .catch((error)=>{
         console.error('Error Signing out:', error);
