1213479af88f2c978c4e9c8f8f387545b7278210
Jeltai <101892796+Jeltai@users.noreply.github.com>
2024-10-08 20:05:12 +0200

    update

--- pages/JS/functions.js
+++ pages/JS/functions.js
@@ -7,5 +7,17 @@ function start_opgave2() {
 }
 
 function naar_theorie_8_1() {
-    window.open("theorie-8.1-A.html");
+    window.open("theorie-8.1.html", 'newwindow', 'width=910,height=800');
+}
+
+function open_theorie(i) {
+    let text = i.toString();
+    var opdracht = "theorie-" + text + ".html";
+    window.open(opdracht, "_self");
+}
+
+function open_theorie_newwindow(i) {
+    let text = i.toString();
+    var opdracht = "theorie-" + text + ".html";
+    window.open(opdracht, 'newwindow', 'width=910,height=780');
 }
