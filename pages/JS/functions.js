d3f389b3b982355c886a6c55d94932760fe276c8
Jeltai <101892796+Jeltai@users.noreply.github.com>
2024-11-09 17:48:45 +0100

    update

--- pages/JS/functions.js
+++ pages/JS/functions.js
@@ -6,26 +6,6 @@ function start_opgave2() {
     window.open("opzet_vraag2.html","_self");
 }
 
-function start_opgave_raaklijn1() {
-    window.open("raaklijn1.html","_self");
-}
-
-function start_opgave_raaklijn2() {
-    window.open("raaklijn2.html","_self");
-}
-
-function start_opgave_raaklijn3() {
-    window.open("raaklijn3.html","_self");
-}
-
-function start_opgave_raaklijn4() {
-    window.open("raaklijn4.html","_self");
-}
-
-function start_opgave_raaklijn5() {
-    window.open("raaklijn5.html","_self");
-}
-
 function naar_theorie_8_1() {
     window.open("theorie-8.1.html", 'newwindow', 'width=910,height=800');
 }
@@ -40,4 +20,10 @@ function open_theorie_newwindow(i) {
     let text = i.toString();
     var opdracht = "theorie-" + text + ".html";
     window.open(opdracht, 'newwindow', 'width=910,height=780');
+}
+
+function ehc_opgave(i) {
+    let text = i.toString();
+    var opdracht = "opzet_vraag" + text + ".html";
+    window.open(opdracht,"_self");
 }
