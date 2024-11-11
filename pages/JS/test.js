06af2de40bd135d400fae94eeaa0aefcad0cad96
Jeltai <101892796+Jeltai@users.noreply.github.com>
2024-11-11 17:39:03 +0100

    test

--- pages/JS/test.js
+++ pages/JS/test.js
@@ -1,7 +1,7 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
  import {getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
- import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+ import{getFirestore, doc, setDoc, getDoc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
  
 
